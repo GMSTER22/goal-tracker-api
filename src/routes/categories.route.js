b8b49a00f8964bc4a5dcc0df1dd113442c41987a
Waterskier74 <90514994+Waterskier74@users.noreply.github.com>
2023-10-13 11:08:49 -0400

    Larrabee category post delete update

--- src/routes/categories.route.js
+++ src/routes/categories.route.js
@@ -5,5 +5,7 @@ const categoryController = require('../controllers/categories.controller');
 router.get('/', categoryController.getAllCategories);
 router.get('/:id', categoryController.getOneCategory);
 router.post('/', categoryController.createCategory);
+router.put('/:id', categoryController.updateCategory);
+router.delete('/:id', categoryController.deleteCategory);
 
 module.exports = router;
--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -5,5 +5,7 @@ const commentController = require('../controllers/comments.controller');
 router.get('/', commentController.getAllComments);
 router.get('/:id', commentController.getOneComment);
 router.post('/', commentController.createComment);
+router.put('/:id', commentController.updateComment);
+router.delete('/:id', commentController.deleteComment);
 
 module.exports = router;
