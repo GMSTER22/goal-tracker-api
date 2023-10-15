1665307469509f07aa5add1f73a96aed64d590a0
edgar <edgardavid1503@gmail.com>
2023-10-14 19:56:13 -0700

    uncomment error handling

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ app.use((err, req, res, next) => {
   res.status(500).send('Internal Server Error!');
 });
 
-// process.on('uncaughtException', (err, origin) => {
-//   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
-// });
+process.on('uncaughtException', (err, origin) => {
+  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
 
 app.use((req, res, next) => {
   res.header({
