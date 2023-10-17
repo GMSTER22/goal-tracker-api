46ed1600d6c77270c021a0b1b83dc8dc2fe32bcf
edgar <edgardavid1503@gmail.com>
2023-10-17 15:18:54 -0700

    corrected typo on userId

--- validation.js
+++ validation.js
@@ -1,19 +1,19 @@
 const { check, validationResult } = require('express-validator');
 
 const categoryValidation = [
-  check('usertId', 'userId cannot be empty').not().isEmpty(),
+  check('userId', 'userId cannot be empty').not().isEmpty(),
   check('categoryName', 'categoryName cannot be empty').not().isEmpty()
 ];
 
 const commentValidation = [
-  check('usertId', 'userId cannot be empty').not().isEmpty(),
+  check('userId', 'userId cannot be empty').not().isEmpty(),
   check('goalId', 'goalId cannot be empty').not().isEmpty(),
   check('text', 'text cannot be empty').not().isEmpty(),
   check('createdAt', 'Please include a valid date (yyyy-mm-dd)').isISO8601().toDate()
 ];
 
 const goalValidation = [
-  check('usertId', 'userId cannot be empty').not().isEmpty(),
+  check('userId', 'userId cannot be empty').not().isEmpty(),
   check('categoryId', 'categoryId cannot be empty').not().isEmpty(),
   check('title', 'title cannot be empty').not().isEmpty(),
   check('description', 'description cannot be empty').not().isEmpty(),
